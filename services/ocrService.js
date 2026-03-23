2b9c4bceb156e7482481e6a5da80fdc9db5ef21b
MAYANK_JHA <jham8364@gmail.com>
2026-03-23 20:01:53 +0530

    Delete services directory

--- services/ocrService.js
+++
@@ -1,9 +0,0 @@
-const Tesseract = require('tesseract.js');
-
-const ocrService = {
-    recognize: async (image) => {
-        return await Tesseract.recognize(image, 'eng');
-    }
-};
-
-module.exports = ocrService;
