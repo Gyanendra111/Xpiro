f534f1ab66e541832d6461072e14e054bce73fc2
MAYANK_JHA <jham8364@gmail.com>
2026-03-23 20:00:57 +0530

    Delete routes directory

--- routes/reminders.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Reminder routes
-router.get('/', (req, res) => {
-    // Get all reminders logic
-});
-
-module.exports = router;
